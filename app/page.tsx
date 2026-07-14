666f592412ec33dc68fb881d564cb57968c703ca
kenihsieh <keni@charge-spot.tw>
2026-07-14 17:13:10 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+      <h1>CHARGESPOT 短網址系統已上線</h1>
+    </div>
+  )
+}
