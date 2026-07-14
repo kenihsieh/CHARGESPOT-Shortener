4752a63f422466974cd76aa74ad30686b8b7a837
kenihsieh <keni@charge-spot.tw>
2026-07-14 17:14:04 +0800

    Create page.tsx

---
+++ app/404/page.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+      <h1>404 - 找不到此短網址</h1>
+      <p>請確認您的網址是否輸入正確。</p>
+    </div>
+  )
+}
